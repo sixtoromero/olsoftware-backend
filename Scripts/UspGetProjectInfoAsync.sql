abacb78e56f266faecda0c6eb24a55bc6183f289
sixtux <sixto.jose@gmail.com>
2021-05-04 17:27:13 -0500

    Se crea Script para el procedimiento almacenado

--- Scripts/UspGetProjectInfoAsync.sql
+++ Scripts/UspGetProjectInfoAsync.sql
@@ -3,3 +3,4 @@ AS
 	SELECT c.Names + ' ' + c.Surnames, c.Phone, p.ProjectName, p.StartDate, p.EndDate, p.Price, p.NumberHours, p.Status  FROM [dbo].[Projects] p 
 	INNER JOIN [dbo].[Customers] c ON p.CustomerId = c.Id
 GO
+
